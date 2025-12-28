4e86d3972a7f7d4c333b59621e35ced8dfa3527f
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-12-29 07:05:32 +0900

    레디스 추가

---
+++ modules/redis/src/main/java/com/dragons/config/redis/RedisProperties.java
@@ -0,0 +1,12 @@
+package com.dragons.config.redis;
+
+import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(value = "datasource.redis")
+public record RedisProperties(
+    int database,
+    RedisNodeInfo master,
+    List<RedisNodeInfo> replicas
+) {
+}
