969ff460c2c2591671e2208ffded8718148276e3
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-12-30 06:33:54 +0900

    add: 계정 기능 - e2e 테스트까지 완료

---
+++ apps/dragons_api/src/main/java/com/dragons/dragons_api/interfaces/user/dto/UserV1Dto.java
@@ -0,0 +1,57 @@
+package com.dragons.dragons_api.interfaces.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+// v1 userDto
+public class UserV1Dto {
+  // 회원가입
+  public static class Register {
+
+    // 요청
+    public record Request(
+
+        @NotBlank(message = "이름은 필수입니다.")
+        @Size(min = 2, max = 20, message = "이름은 2~20자여야 합니다.")
+        String name,
+
+        @NotBlank(message = "이메일은 필수입니다.")
+        @Email(message = "유효한 이메일 형식이 아닙니다.")
+        String email,
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+        @Pattern(
+            regexp = "^(?:(?=.*[A-Za-z])(?=.*\\d)|(?=.*[A-Za-z])(?=.*[^A-Za-z0-9])|(?=.*\\d)(?=.*[^A-Za-z0-9]))[A-Za-z\\d[^A-Za-z0-9]]+$",
+            message = "비밀번호는 영문, 숫자, 특수문자 중 2가지 이상을 포함해야 합니다."
+        )
+        String password
+    ) {
+
+
+    }
+
+    // 응답
+    public record Response(
+        String name,
+        String email
+    ) {
+
+    }
+  }
+
+  // 로그인
+  public static class Login {
+
+    public record Request(String email, String password) {
+
+    }
+
+    public record Response(String token) {
+
+    }
+
+  }
+}
