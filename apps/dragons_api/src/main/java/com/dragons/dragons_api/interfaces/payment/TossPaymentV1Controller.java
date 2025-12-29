282caf7a5a29d60a0ef82d8894fcf8260493e16f
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-12-30 08:07:56 +0900

    feat: 토스 결제 껍데기 controller 작성

---
+++ apps/dragons_api/src/main/java/com/dragons/dragons_api/interfaces/payment/TossPaymentV1Controller.java
@@ -0,0 +1,27 @@
+package com.dragons.dragons_api.interfaces.payment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments/toss")
+public class TossPaymentV1Controller {
+
+  @PostMapping
+  public void request() {
+
+  }
+
+  @GetMapping("/success")
+  public void success() {
+
+  }
+
+  @GetMapping("/fail")
+  public void fail() {
+
+  }
+
+}
