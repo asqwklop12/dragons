b849ca00ef4f6c2e13f310978f362e3947ab37ae
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2025-12-31 10:55:55 +0900

    fix: config 수정

--- modules/jpa/src/main/java/com/dragons/config/jpa/JpaConfig.java
+++ modules/jpa/src/main/java/com/dragons/config/jpa/JpaConfig.java
@@ -1,13 +1,11 @@
 package com.dragons.config.jpa;
 
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@EntityScan({"com.dragons"})
 @EnableJpaRepositories({"com.dragons"})
 public class JpaConfig {
 }
