28119455a22312aaa9db8c85be0f021503df7488
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-01-07 19:59:47 +0900

    fix:스프링 부트실행시키는 경우 log파일이 만들어지지 않은 이슈 수정

--- apps/dragons_api/src/test/java/com/dragons/interfaces/api/user/UserV1ControllerE2ETest.java
+++ apps/dragons_api/src/test/java/com/dragons/interfaces/api/user/UserV1ControllerE2ETest.java
@@ -9,12 +9,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
--- infra/jpa/src/main/java/com/dragons/infra/jpa/post/JpaPostRepository.java
+++ infra/jpa/src/main/java/com/dragons/infra/jpa/post/JpaPostRepository.java
@@ -1,9 +1,9 @@
 package com.dragons.infra.jpa.post;
 
+import com.dragons.domain.post.Post;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import com.dragons.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface JpaPostRepository extends JpaRepository<Post, Long> {
