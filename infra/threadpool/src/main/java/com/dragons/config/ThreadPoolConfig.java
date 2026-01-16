785d7138c499d51819921fb30e70d5ed85d2acd2
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-01-16 23:38:05 +0900

    refactor:  거부 정책은 기본값으로

--- infra/threadpool/src/main/java/com/dragons/config/ThreadPoolConfig.java
+++ infra/threadpool/src/main/java/com/dragons/config/ThreadPoolConfig.java
@@ -41,7 +41,6 @@ public class ThreadPoolConfig {
     executor.setThreadNamePrefix(threadNamePrefix);
     executor.setKeepAliveSeconds(Math.toIntExact(keepAlive.getSeconds()));
 
-    // 큐가 가득 찼을 때 호출한 스레드에서 직접 실행하여 back-pressure 제공
     executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
 
     // 애플리케이션 종료 시 작업 완료 대기
