6d7275715b2bcfc62fa690a85b53b9bdeed6b869
asqwklop12 <33236685+asqwklop12@users.noreply.github.com>
2026-01-28 14:39:15 +0900

    feat: Qualifier 적용

--- infra/retry/src/main/java/com/dragons/config/RetryConfig.java
+++ infra/retry/src/main/java/com/dragons/config/RetryConfig.java
@@ -5,6 +5,7 @@ import com.dragons.policy.DefaultRetryPolicy;
 import com.dragons.policy.RetryPolicy;
 import com.dragons.policy.SocialLoginRetryPolicy;
 import com.dragons.properties.RetryProperties;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,7 +33,7 @@ public class RetryConfig {
   }
 
   @Bean
-  public RetryExecutor socialRetryExecutor(RetryPolicy retrySocialPolicy) {
+  public RetryExecutor socialRetryExecutor(@Qualifier("retrySocialPolicy") RetryPolicy retrySocialPolicy) {
     return new RetryExecutor(retrySocialPolicy);
   }
 
